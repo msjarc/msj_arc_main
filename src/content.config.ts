19995a0649507147d1860bc2be31031233a6659d
ingsoc <84819314+ingsocSecrets@users.noreply.github.com>
2026-07-10 21:50:10 -0700

    WIP: implemented new author system; now supported in sveltiacms, profile pictures, astro collections definitions updated, pageheader updated to properly display avatar image, two basic author profiles added

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,41 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, reference, z } from 'astro:content'
 
 // 2. Import loader(s)
 import { glob } from 'astro/loaders'
 
 // 3. Define your collection(s)
-const projects = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/content/projects' }),
+const authors = defineCollection({
+  loader: glob({ base: './src/data/authors', pattern: '**/*.json' }),
   schema: z.object({
     title: z.string(),
-    author: z.string(),
+    bio: z.string().optional(),
+    image: z.string().optional(),
+  })
+
+})
+
+const blog = defineCollection({
+  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+  schema: z.object({
+    title: z.string(),
+    author: reference('authors'),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    // updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).default([]),
+  })
+})
+
+const activities = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/activities' }),
+  schema: z.object({
+    title: z.string(),
+    author: reference('authors'),
     description: z.string(),
     tags: z.array(z.string()).default([]),
   }),
 })
 
 // 4. Export a single `collections` object to register you collection(s)
-export const collections = { projects }
+export const collections = { authors, blog, activities }
